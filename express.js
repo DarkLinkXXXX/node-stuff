2f3069be6d8edaa8ec4c05b10a042cc6eb5945f1
unknown <Jeff@Jeff-PC.(none)>
2011-12-27 14:59:45 -0800

    Made express.js more verbose.

--- express.js
+++ express.js
@@ -2,10 +2,12 @@ var app = require('express').createServer();
 
 app.get('/', function(req, res){
 	res.send('Hello W0rld');
+	console.log('Hello W0rld');
 });
 
 app.get('/user/:id', function(req, res){
-	res.send('Hello ' + req.params.id)
+	res.send('Hello ' + req.params.id);
+	console.log('Hello' + req.params.id);
 });
 
 app.listen(3000)
